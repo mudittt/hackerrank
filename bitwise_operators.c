f1bc53fcf0cab5837a12128dbd7ce161cec8a2a8
Vaibhav Upreti <85568177+VaibhavUpreti@users.noreply.github.com>
2022-01-05 18:21:28 +0530

    Just removed some extra spacing
    Saves memory.. :)

--- bitwise_operators.c
+++ bitwise_operators.c
@@ -2,9 +2,6 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-
-
-
 void calculate_the_maximum(int n, int k) {
     int arr[n];
     int temp1 = (0);
@@ -15,48 +12,35 @@ void calculate_the_maximum(int n, int k) {
     }
     for(int j= 0; j < k; j++){
         for (int i =0; i < (n-1); i++ ) {
-            if(j < (i+1)){
-            
-                if(((arr[j] & arr[i+1]) > temp1) && ((arr[j] & arr[i+1]) < k)){
-                    
-                    temp1 = (arr[j] & arr[i+1]);
-                    
+            if(j < (i+1)){           
+                if(((arr[j] & arr[i+1]) > temp1) && ((arr[j] & arr[i+1]) < k)){                  
+                    temp1 = (arr[j] & arr[i+1]);                  
                 }
             }
-        }
-        
+        }     
         for (int i =0; i < (n-1); i++ ) {
             if(j < (i+1)){
-                if(((arr[j] | arr[i+1]) > temp2) && ((arr[j] | arr[i+1]) < k)){
-                    
+                if(((arr[j] | arr[i+1]) > temp2) && ((arr[j] | arr[i+1]) < k)){                   
                     temp2 = (arr[j] | arr[i+1]);
                     
                 }
             }
-        }
-        
+        }        
         for (int i =0; i < (n-1); i++ ) {
             if(j < (i+1)){
                 if(((arr[j] ^ arr[i+1]) > temp3) && ((arr[j] ^ arr[i+1]) < k)){
-                    
-                    temp3 = (arr[j] ^ arr[i+1]);
-                    
+                    temp3 = (arr[j] ^ arr[i+1]);           
                 }
             }
-        }
-    
+        }    
     }
     printf("%d\n", temp1);
     printf("%d\n", temp2);
     printf("%d\n", temp3);
-  
 }
-
 int main() {
     int n, k;
-  
     scanf("%d %d", &n, &k);
     calculate_the_maximum(n, k);
- 
     return 0;
 }
